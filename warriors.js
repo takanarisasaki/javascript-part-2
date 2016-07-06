84ab43e809bd7d44df2f2bad978f01f468f13d17
Takanari Sasaki <takanari.s@hotmail.com>
2016-07-06 21:16:12 +0000

    Warriors fight

--- warriors.js
+++ warriors.js
@@ -11,3 +11,28 @@ function Warrior(name, gender) {
 var maleWarrior = new Warrior("John", "M");
 var femaleWarrior = new Warrior("Sally", "F");
 console.log(maleWarrior);
+console.log(femaleWarrior);
+
+function fight(warrior) {
+    console.log(warrior.name +  " rushes to the arena with " + warrior.weapon);
+    
+    return function(opponents) {
+        if (warrior.power > opponents.power) {
+            console.log("The warrior's attack is " + warrior.power);
+            console.log("The opponent's attack is " + opponents.power);
+            console.log(warrior.name + " wins!");
+        }
+        else if(warrior.power < opponents.power) {
+            console.log("The warrior's attack is " + warrior.power);
+            console.log("The opponent's attack is " + opponents.power);
+            console.log("Opponent warrior wins!");
+        }
+        else if (warrior.power === opponents.power) {
+            console.log("The warrior's attack is " + warrior.power);
+            console.log("The opponent's attack is " + opponents.power);
+            console.log("Draw!");
+        }
+    }
+}
+
+fight(maleWarrior)(femaleWarrior);
